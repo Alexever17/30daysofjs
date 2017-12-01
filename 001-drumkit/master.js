5a630f317bf72445f5857d75d35301b1883892b4
alexever17@gmail.com <alexever17@gmail.com>
2017-12-01 01:48:13 +0100

    basic functionality without the light animation

---
+++ 001-drumkit/master.js
@@ -0,0 +1,81 @@
+
+window.addEventListener("keypress", function(event){
+  var x = event.charCode;
+  soundDecider(x);
+});
+
+function soundDecider(number) {
+  var audioID = "";
+  var keyID = "";
+
+  switch (number) {
+    case 97:
+        audioID = "eins";
+        keyID = "A";
+        playingSound(audioID);
+        animateButton(keyID);
+        break;
+    case 115:
+        audioID = "zwei";
+        keyID = "S";
+        playingSound(audioID);
+        animateButton(keyID);
+        break;
+    case 100:
+        audioID = "drei";
+        keyID = "D";
+        playingSound(audioID);
+        animateButton(keyID);
+        break;
+    case 102:
+        audioID = "vier";
+        keyID = "F";
+        playingSound(audioID);
+        animateButton(keyID);
+        break;
+    case 32:
+        audioID = "fuenf";
+        keyID = "Space";
+        playingSound(audioID);
+        animateButton(keyID);
+        break;
+    case 104:
+        audioID = "sechs";
+        keyID = "H";
+        playingSound(audioID);
+        animateButton(keyID);
+        break;
+    case 106:
+        audioID = "sieben";
+        keyID = "J";
+        playingSound(audioID);
+        animateButton(keyID);
+        break;
+    case 107:
+        audioID = "acht";
+        keyID = "K";
+        playingSound(audioID);
+        animateButton(keyID);
+        break;
+    case 108:
+        audioID = "neun";
+        keyID = "L";
+        playingSound(audioID);
+        animateButton(keyID);
+        break;
+      }
+}
+
+function playingSound(keytrigger) {
+    var treck = document.getElementById(keytrigger);
+    treck.currentTime = 0;
+    treck.play();
+}
+
+function animateButton(Letter) {
+    var onscreenKEY = document.getElementById(Letter);
+    onscreenKEY.classList.add('playing');
+    onscreenKEY.classList.remove('playing');
+    //remove('MyClass');
+
+}
